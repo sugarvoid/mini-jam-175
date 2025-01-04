8c795738f1d46e2365be016f798c4b15c50e188d
Dave <gamedev@kitargo.com>
2025-01-03 18:46:55 -0600

    Add plane x movement

--- main.lua
+++ main.lua
@@ -1,40 +1,38 @@
 gamestates = {
     title = 0,
-    day_intro = 1,
-    day_title = 2,
     game = 3,
-    bonus = 4,
     gameover = 5
 }
 g_state = gamestates.game
 
-frame_x = {8, 32, 56, 80, 104, 80, 56, 32, 8}
-x_speeds = {}
-y_speeds = {}
+frame_x = { 8, 32, 56, 80, 104, 80, 56, 32, 8 }
+dx = { -0.4, -0.3, -0.2, -0.1, 0, 0.1, 0.2, 0.3, 0.4 }
+y_speeds = { 0.2, 0.3, 0.4, 0.5, 1, 0.5, 0.4, 0.3, 0.2 }
 
 function _init()
     p1 = {
-        x =30,
-        y = 50,
+        x = 48,
+        y = 20,
         facing_right = false,
-        frame=1,
-        fake_frame=0,
-        update=function(self)
+        frame = 1,
+        fake_frame = 0,
+        update = function(self)
             self.facing_right = self.frame > 5
-            self.y+=.05
+            self.y += y_speeds[p1.frame]
+            p1.x = mid(16, p1.x + dx[p1.frame], 64)
         end,
-        update_img=function(self, dir)
+        update_img = function(self, dir)
             if dir == "l" then
-                self.frame-=1
-               -- self.frame = self.fake_frame
+                self.frame -= 1
+                -- self.frame = self.fake_frame
             else
-                self.frame+=1
+                self.frame += 1
                 --if self.frame > 5 then
-                    --self.frame = (4 * self.fake_frame)
-                    --self.facing_right = true
-                   -- else
-                 --       self.facing_right = false 
-               -- end
+                --self.frame = (4 * self.fake_frame)
+                --self.facing_right = true
+                -- else
+                --       self.facing_right = false
+                -- end
             end
         end,
     }
@@ -53,22 +51,18 @@ end
 function _draw()
     cls()
     map()
-        pal(14, 0)
-        sspr(frame_x[p1.frame], 0, 24, 24, p1.x, p1.y, 24, 24, p1.facing_right)
-        pal()
- 
-    
+    pal(14, 0)
+    sspr(frame_x[p1.frame], 0, 24, 24, p1.x, p1.y, 24, 24, p1.facing_right)
+    pal()
+
+
     print(p1.frame, 8, 8, 7)
 end
 
 function check_inputs()
     if btnp(🅾️) then
         if g_state == gamestates.title then
-            g_state = gamestates.day_intro
-        elseif g_state == gamestates.day_intro then
-            intro_t = 0
-        elseif g_state == gamestates.day_title then
-            day_t = 0
+            g_state = gamestates.game
         elseif g_state == gamestates.game then
 
         elseif g_state == gamestates.gameover then
@@ -77,10 +71,6 @@ function check_inputs()
     end
     if btnp(❎) then
         if g_state == gamestates.title then
-
-        elseif g_state == gamestates.day_intro then
-
-        elseif g_state == gamestates.day_title then
 
         elseif g_state == gamestates.game then
 
@@ -93,9 +83,6 @@ function check_inputs()
     if btnp(➡️) then
         if g_state == gamestates.title then
 
-        elseif g_state == gamestates.day_intro then
-
-        elseif g_state == gamestates.day_title then
 
         elseif g_state == gamestates.game then
             --p1:update_img("r")
@@ -108,10 +95,6 @@ function check_inputs()
         end
     elseif btnp(⬅️) then
         if g_state == gamestates.title then
-
-        elseif g_state == gamestates.day_intro then
-
-        elseif g_state == gamestates.day_title then
 
         elseif g_state == gamestates.game then
             p1.frame = mid(1, p1.frame - 1, 9)
